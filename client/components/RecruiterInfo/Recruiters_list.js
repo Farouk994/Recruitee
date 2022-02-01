4fdb34318eb56b7b2f7355c010b92caae3391a02
Farouk <faroukkisuule55@gmail.com>
2022-02-01 15:21:01 -0500

    Separated about component from the jobs page to maximize the use of React props then passed data to it to list recruiters

--- client/components/RecruiterInfo/Recruiters_list.js
+++ client/components/RecruiterInfo/Recruiters_list.js
@@ -4,7 +4,7 @@ const Recruiters_list = () => {
    return (
       <div>
          <div class="px-8">
-            <h1 class="mb-4 text-xl font-bold text-gray-700">Authors</h1>
+            <h1 class="mb-4 text-xl font-bold text-gray-700">Recruiters</h1>
             <div class="flex flex-col max-w-sm px-6 py-4 mx-auto bg-white rounded-lg shadow-md">
                <ul class="-mx-4">
                   <li class="flex items-center">
